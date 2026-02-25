91d055e69b5abadff419ce011875ade3580606fb
Kostas <kostliutk2@gmail.com>
2026-02-25 23:53:29 +0200

    rough nav prep + comp structure

---
+++ project-ui/app/products/products.tsx
@@ -0,0 +1,4 @@
+export function Products() {
+  return ( 
+<h1>Main content of products component</h1>);
+}
--- project-ui/app/routes/home.tsx
+++ project-ui/app/routes/home.tsx
@@ -1,13 +1,20 @@
 import type { Route } from "./+types/home";
 import { Welcome } from "../welcome/welcome";
+import { Products } from "../products/products";
+import { NavBar } from "../navBar/navBar";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "Buy Games, Gift Cards & Top Ups Cheaper" },
+    { name: "description", content: "Eneba — the fastest-growing marketplace for gamers. Get games and gift cards for PC, PlayStation, Xbox, Nintendo in safe and affordable way. Eneba way." },
   ];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return (
+    <div>
+      <NavBar/>
+      <Products />
+    </div>
+    );
 }
